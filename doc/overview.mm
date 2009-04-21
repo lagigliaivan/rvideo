77a4b5bb49b0abd5ecb59210b8395efc30c9f6e8
ivan <lagigliaivan@gmail.com>
2009-04-21 15:41:50 -0300

    I added a mindmap graph in order to clarify some ideas and where I want to go.

---
+++ doc/overview.mm
@@ -0,0 +1,44 @@
+<map version="0.7.1">
+<node TEXT="rvideo">
+<node TEXT="access" POSITION="right">
+<node TEXT="We should have a couple of access methos. They can be">
+<node TEXT="to the cameras throught http"/>
+<node TEXT="to an administrative page throught http"/>
+<node TEXT="directly to the sip proxy through the desktop application">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+<node TEXT="protocols" POSITION="left">
+<node TEXT="RTP"/>
+<node TEXT="SIP"/>
+<node TEXT="UDP"/>
+<node TEXT="HTTP"/>
+<node TEXT="SHTTP"/>
+</node>
+<node TEXT="bugs" POSITION="right">
+<icon BUILTIN="clanbomber"/>
+<node TEXT="There are a lot of bugs."/>
+<node TEXT="Some of them are not found yet."/>
+<node TEXT="We have to know when stop debugging and start coding the new features needed">
+<icon BUILTIN="attach"/>
+</node>
+</node>
+<node TEXT="improvements" POSITION="left">
+<node COLOR="#990000" TEXT="HTTP Access instead of accessing from a desktop application.">
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="IP cameras connected to an embedded device instead of webcams connected to desktop">
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="Allow sharing between different users.">
+<icon BUILTIN="ksmiletris"/>
+</node>
+</node>
+<node TEXT="source code" POSITION="right">
+<node TEXT="versioning throught git"/>
+<node TEXT="acomplish good programming technics"/>
+<node TEXT="performance is needed"/>
+</node>
+</node>
+</map>
